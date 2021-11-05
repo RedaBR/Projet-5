9cb7236e4326c40b8225af9cad96996af392a184
Reda <>
2021-11-05 10:06:19 +0100

    test

--- CountOnMeTests/ElementsTest.swift
+++ CountOnMeTests/ElementsTest.swift
@@ -1,23 +1,52 @@
 //
-//  SimpleCalcTests.swift
-//  SimpleCalcTests
+//  ElementsTest.swift
+//  CountOnMeTests
 //
-//  Created by Vincent Saluzzo on 29/03/2019.
-//  Copyright © 2019 Vincent Saluzzo. All rights reserved.
+//  Created by Reda on 05/11/2021.
+//  Copyright © 2021 Vincent Saluzzo. All rights reserved.
 //
-
 import XCTest
 @testable import CountOnMe
 
-class ElementsTest : XCTestCase {
+class ElementsTest: XCTestCase {
 
-    func testGivenElementsIsEmpty_WhenElementContainsOperations_ThenCorrectResultAffiched () {
-            let objects = Elements()
-        objects.elements = [" 1 + 1 = "]
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
 
-        _ = objects.calculate()
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+
+            func testGivenElementsIsEmpty_WhenElementContainsOperations_ThenCorrectResultAffiched () {
+                    let objects = Elements()
+                objects.elements = [" 1 + 1 = "]
+                
+                objects.updateElementsChoice(text: " 1 + 1 = ")
+                if objects.expressionHaveResult() {
+                    if objects.expressionHaveEnoughElement() {
+                        let result = objects.calculate()
+                        XCTAssertEqual(result, "2")
+                    } else {
+                        XCTAssertFalse(objects.expressionHaveEnoughElement())
+                    }
+                    
+                } else {
+                    XCTAssertFalse(objects.expressionHaveResult())
+                }
+            
         
-        XCTAssert(objects.operationsToReduce.contains("2"))
-}
+        }
+
+            }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
 
 }
